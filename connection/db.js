47ca5fd439017bc8451765e255bc77e4aab02e40
akhildileep1997 <akhildileep1997@gmail.com>
2024-06-23 09:10:59 +0530

    change in root server side

--- connection/db.js
+++ connection/db.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 const connectDb = mysql.createConnection({
-  host: "localhost",
+  host: "172.31.44.203",
   user: "root",
   password: "A9846@khil#",
   database: "user-details",
@@ -9,7 +9,7 @@ const connectDb = mysql.createConnection({
 
 connectDb.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database:",err.message);
+    console.error("Error connecting to the database:", err.message);
     return;
   }
   console.log("Database connection established successfully");
